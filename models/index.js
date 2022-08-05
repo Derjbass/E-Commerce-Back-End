3c577fde4e1daeb87e5c9a38464b472db9784010
Derjbass <Derek.j.bass@gmail.com>
2022-08-05 16:41:34 -0500

    added reference for tables through index.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,16 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category);
 
 // Categories have many Products
+Category.hasMany(Product)
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, ProductTag, { foreignKey: 'product_id', targetKey: 'id'})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, ProductTag, { foreignKey: 'tag_id', targetKey: 'id'})
 
 module.exports = {
   Product,
